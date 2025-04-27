43156f1b7ab90cf051018dafc1389238b2909777
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-04-27 17:45:43 +0200

    First version

---
+++ Const.pm
@@ -0,0 +1,35 @@
+package Data::Const;
+
+use strict;
+use warnings;
+
+use Class::Utils qw(set_params);
+use Error::Pure qw(err);
+
+our $VERSION = 0.01;
+
+# Constructor.
+sub new {
+	my ($class, @params) = @_;
+
+	# Create object.
+	my $self = bless {}, $class;
+
+	# Mode to present ids.
+	$self->{'mode_id'} = 1;
+
+	# Process parameters.
+	set_params($self, @params);
+
+	return $self;
+}
+
+sub data {
+	my $self = shift;
+
+	err "Need to be implemented.";
+}
+
+1;
+
+__END__
