d605d70c90cc493d6db6a5407045644a27283c0a
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-09-15 22:25:47 +0200

    Remove 'object_isa' parameter

--- Const.pm
+++ Const.pm
@@ -5,8 +5,6 @@ use warnings;
 
 use Class::Utils qw(set_params);
 use Error::Pure qw(err);
-use Mo::utils 0.01 qw(check_required);
-use Scalar::Util qw(blessed);
 
 our $VERSION = 0.02;
 
@@ -17,34 +15,13 @@ sub new {
 	# Create object.
 	my $self = bless {}, $class;
 
-	# Output ISA.
-	$self->{'output_isa'} = undef;
-
 	# Process parameters.
 	set_params($self, @params);
-
-	# Check 'output_isa';
-	check_required($self, 'output_isa');
 
 	return $self;
 }
 
 sub data {
-	my $self = shift;
-
-	my $obj = $self->_data;
-
-	if (! defined $obj
-		|| ! blessed($obj)
-		|| ! $obj->isa($self->{'output_isa'})) {
-
-		err 'Bad output object.';
-	}
-
-	return $obj;
-}
-
-sub _data {
 	my $self = shift;
 
 	err "Need to be implemented.";
--- t/Data-Const/03-new.t
+++ t/Data-Const/03-new.t
@@ -6,7 +6,5 @@ use Test::More 'tests' => 2;
 use Test::NoWarnings;
 
 # Test.
-my $obj = Data::Const->new(
-	'output_isa' => 'Foo::Bar',
-);
+my $obj = Data::Const->new;
 isa_ok($obj, 'Data::Const');
